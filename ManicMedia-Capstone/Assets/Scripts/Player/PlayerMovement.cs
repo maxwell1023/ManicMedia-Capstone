23cdbc295be36c44a4960f6144cb4d4210455679
maxwell1023 <maxwelle1023@gmail.com>
2023-05-29 02:02:23 -0400

    Main Movement done (no jump)
    Still need to add the jump and tweak the camera in the over-the-shoulder mode

--- ManicMedia-Capstone/Assets/Scripts/Player/PlayerMovement.cs
+++ ManicMedia-Capstone/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float playerSpeed = 1f, aimSpeed = .5f;
+    private float playerSpeed = 1f, aimSpeed = .5f, rotationSpeed = .8f, mainTurnSpeed = 2;
     [SerializeField]
     private CinemachineVirtualCamera holdingCam;
-
-    private Transform cameraTransform;
+    [SerializeField]
+    private GameObject hitMarker;
+   
 
     private bool mainCameraOn = true;
     private bool cameraDelayed = true;
-
+    float turnV;
     private void Start()
     {
+        hitMarker.SetActive(false);
         mainCameraOn = true;
         cameraDelayed = true;
     }
@@ -30,7 +33,16 @@ public class PlayerMovement : MonoBehaviour
 
         if(mainCameraOn == true)
         {
-            MainMove();
+            
+            if((Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow)) || (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)))
+            {
+                print("STOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOPSTOP");
+
+            }
+            else
+            {
+                MainMove();
+            }
         }
         else if (mainCameraOn == false) 
         {
@@ -38,13 +50,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-
-   private void MainMove()
+        private void MainMove()
     {
         float playerVertical = Input.GetAxis("Vertical");
         float playerHorizontal = Input.GetAxis("Horizontal");
 
-       
 
         Vector3 forward = Camera.main.transform.forward;
         Vector3 right = Camera.main.transform.right;
@@ -59,12 +69,31 @@ public class PlayerMovement : MonoBehaviour
         Vector3 cameraAdjustedMovement = forwardFromCam + rightFromCam;
         this.transform.Translate(cameraAdjustedMovement * playerSpeed*Time.deltaTime, Space.World);
 
+        if (cameraAdjustedMovement != Vector3.zero)
+        {
+            transform.forward = cameraAdjustedMovement;
+        }
+       
+
     }
 
     private void AimMove()
     {
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward = forward.normalized;
+        right = right.normalized;
+
         Vector3 totalMovement = new Vector3 (Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        totalMovement = totalMovement.x * right + totalMovement.z * forward;
         this.transform.Translate(totalMovement * aimSpeed * Time.deltaTime, Space.World);
+
+        float cameraAngle = Camera.main.transform.rotation.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0,cameraAngle,0);    
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
     }
 
    private void SwitchCamera()
@@ -74,10 +103,12 @@ public class PlayerMovement : MonoBehaviour
             if (mainCameraOn == true)
             {
                 holdingCam.Priority = 2;
+                hitMarker.SetActive(true);
             }
             else
             {
                 holdingCam.Priority = 0;
+                hitMarker.SetActive(false);
             }
 
             mainCameraOn = !mainCameraOn;
