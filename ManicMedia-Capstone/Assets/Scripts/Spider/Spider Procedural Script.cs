58aa9b543c0e717895650deab32f3ab43eb242dd
maxwell1023 <123422476+maxwell1023@users.noreply.github.com>
2023-06-13 01:46:08 -0700

    some changes to procedural anim

--- ManicMedia-Capstone/Assets/Scripts/Spider/Spider Procedural Script.cs
+++ ManicMedia-Capstone/Assets/Scripts/Spider/Spider Procedural Script.cs
@@ -13,89 +13,27 @@ public class SpiderProceduralScript : MonoBehaviour
     private float matchLimit = 2f;
     [SerializeField]
     private float legSpeed = 20f;
+    [SerializeField]
+    private float legVertSpeed = 60f;
+    [SerializeField]
+    private float legStepHeight = 2f;
 
     [SerializeField]
     private Transform posCheck01, posCheck02, posCheck03, posCheck04, posCheck05, posCheck06;
-    //private Transform predictedPos01, predictedPos01, predictedPos01, predictedPos01, predictedPos01, predictedPos01,
+  
+    [SerializeField]
+    private Transform[] checkArray = new Transform[6]; 
 
-    private Transform[] checkArray = new Transform[6]; //we could declare these as arrays first rather than a bunch of individual transforms to array
-
-    public Transform Leg01_Target; //can be serialized
-    public Transform Leg02_Target;
-    public Transform Leg03_Target;
-    public Transform Leg04_Target;
-    public Transform Leg05_Target;
-    public Transform Leg06_Target;
-
-    private Transform[] targetArray = new Transform[6]; //we could declare these as arrays first rather than a bunch of individual transforms to array
-
-    public Vector3 initLeg01Pos; //can probably also be serialized
-    public Vector3 initLeg02Pos;
-    public Vector3 initLeg03Pos;
-    public Vector3 initLeg04Pos;
-    public Vector3 initLeg05Pos;
-    public Vector3 initLeg06Pos;
-
-   /* public Vector3 lastLeg01Pos; //same here
-    public Vector3 lastLeg02Pos;
-    public Vector3 lastLeg03Pos;
-    public Vector3 lastLeg04Pos;
-    public Vector3 lastLeg05Pos;
-    public Vector3 lastLeg06Pos; */
+    [SerializeField]
+    private Transform[] targetArray = new Transform[6]; 
 
     private Vector3[] lastLegPosArray = new Vector3[6];
 
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        targetArray[0] = Leg01_Target;
-        targetArray[1] = Leg02_Target;
-        targetArray[2] = Leg03_Target;
-        targetArray[3] = Leg04_Target;
-        targetArray[4] = Leg05_Target;
-        targetArray[5] = Leg06_Target;
-
-        checkArray[0] = posCheck01;
-        checkArray[1] = posCheck02;
-        checkArray[2] = posCheck03;
-        checkArray[3] = posCheck04;
-        checkArray[4] = posCheck05;
-        checkArray[5] = posCheck06;
-
-
-        initLeg01Pos = Leg01_Target.localPosition;
-        initLeg02Pos = Leg02_Target.localPosition;
-        initLeg03Pos = Leg03_Target.localPosition;
-        initLeg04Pos = Leg04_Target.localPosition;
-        initLeg05Pos = Leg05_Target.localPosition;
-        initLeg06Pos = Leg06_Target.localPosition;
-
-        lastLegPosArray[0] = Leg01_Target.position;
-        lastLegPosArray[1] = Leg02_Target.position;
-        lastLegPosArray[2] = Leg03_Target.position;
-        lastLegPosArray[3] = Leg04_Target.position;
-        lastLegPosArray[4] = Leg05_Target.position;
-        lastLegPosArray[5] = Leg06_Target.position;
-    }
-
     // Update is called once per frame
     void Update()
     {
-      /*Leg01_Target.position = lastLegPosArray[0];
-        Leg02_Target.position = lastLegPosArray[1];
-        Leg03_Target.position = lastLegPosArray[2];
-        Leg04_Target.position = lastLegPosArray[3];
-        Leg05_Target.position = lastLegPosArray[4];
-        Leg06_Target.position = lastLegPosArray[5]; */
-
-       /* lastLegPosArray[0] = Leg01_Target.position;
-        lastLegPosArray[1] = Leg02_Target.position;
-        lastLegPosArray[2] = Leg03_Target.position;
-        lastLegPosArray[3] = Leg04_Target.position;
-        lastLegPosArray[4] = Leg05_Target.position;
-        lastLegPosArray[5] = Leg06_Target.position; */
+      
 
         for (int i = 0; i< targetArray.Length; i++) 
         {
@@ -115,16 +53,16 @@ public class SpiderProceduralScript : MonoBehaviour
             {
                 float step = legSpeed * Time.deltaTime;
                 thisTarget.position = Vector3.MoveTowards(thisTarget.position, hit.point, step);
+                
+
             }
-            else if(distance < 6)
+
+            else if(distance < matchLimit)
             {
 
                 thisTarget.position = thisLastPosition;
 
-                
             } 
-
-            Debug.DrawLine(thisLeg.position, hit.point, Color.cyan);
         }
     }
 }
