188ab2cf6a72382c11f90ce4e78bcb83eae7adaa
ChaosQueenGod <daguytoday@gmail.com>
2023-06-25 00:46:02 -0400

    boss testing stuff

---
+++ ManicMedia-Capstone/Assets/Scripts/Hazards/BossProjectile.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossProjectile : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private Vector3 goalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        goalPos = player.transform.position;
+        transform.LookAt(goalPos);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, goalPos, Time.deltaTime * 3.5f);
+
+        if (Vector3.Distance(transform.position, goalPos) <= 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
